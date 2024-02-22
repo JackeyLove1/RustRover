3817767830ee6aa4b0bf914fccd242f03277b650
fanhuanjie <fanhuanjie@bytedance.com>
2024-02-22 23:07:41 +0800

    chore: add iter test

--- src/practice/utils.rs
+++ src/practice/utils.rs
@@ -20,4 +20,50 @@ mod tests{
         let random_f64s: Vec<f64> = generate_random_vec(10);
         println!("Random f64s: {:?}", random_f64s);
     }
+
+    #[test]
+    fn test_vec_iter(){
+        let vec: Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10];
+
+        // find
+        let res1 = vec.iter().find(|&n| n % 2 == 0);
+        assert_eq!(*res1.unwrap(), 2);
+
+        // all
+        let res2 = vec.iter().all(|&n| n < 100);
+        assert_eq!(res2, true);
+
+        // any
+        let res2 = vec.iter().any(|&n| n > 0);
+        assert_eq!(res2, true);
+        
+        // count
+        let res3 = vec.iter().count();
+        assert_eq!(res3, 10);
+
+        // filter
+        let res4: Vec<_> = vec.iter().filter(|&n| n % 2 == 0).collect();
+        assert_eq!(res4, vec![&2, &4, &6, &8, &10]);
+
+        // rev
+        let res5: Vec<_> = vec.iter().rev().collect();
+        assert_eq!(res5, vec![&10, &9, &8, &7, &6, &5, &4, &3, &2, &1]);
+
+        // clone
+        let res6 = vec.iter().clone();
+        assert_eq!(res6, vec![1,2,3,4,5,6,7,8,9,10]);
+
+        // max
+        let res6 = vec.iter().max();
+        assert_eq!(*res6.unwrap(), 10);
+
+        // min
+        let res7 = vec.iter().min();
+        assert_eq!(*res7.unwrap(), 1);
+
+        // sum
+        let res8 = vec.iter().sum();
+        assert_eq!(res8, 55);
+
+    }
 }
